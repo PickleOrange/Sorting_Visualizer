67c2691464684e5a6fff3893c0b1e99e73ea6833
jianhua zhu <jianhuazhu@AAAAA.local>
2023-05-11 13:54:52 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer.js'
+
+function App() {
+  return (
+    <div>
+      <SortingVisualizer />
+    </ div>
+  );
+}
+
+export default App;
---
+++ src/SortingAlgorithms/MergeSort.js
@@ -0,0 +1,11 @@
+export default function MergeSort(array){
+    let arr = array.slice(0);
+    let animations = [];
+    for (let i = 0; i = array.length - 1; i++){
+        if (array[i] < array[i+1]){
+            animations.push()
+        }
+        
+    }
+    return animations
+}
